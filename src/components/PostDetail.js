8da4adeee56619b94069b29d6455b2a2a631e83d
Guilherme Gomes <guifariasgomes@gmail.com>
2024-07-04 16:16:36 -0300

    index footer navbar dashboard post edit text

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,3 +1,5 @@
+// Página de detalhes de um post
+
 import { Link } from "react-router-dom";
 
 import styles from "./PostDetail.module.css";
@@ -7,7 +9,8 @@ const PostDetail = ({ post }) => {
     <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
       <h2>{post.title}</h2>
-      <p className={styles.createdby}>Serviço pelo: <strong>{post.selectedEmployee}</strong></p>
+      <p>Cliente: <strong>{post.createdBy}</strong></p>
+      <p>Funcionário: <strong>{post.selectedEmployee}</strong></p>
       <p>Horário: <strong>{post.time}</strong></p>
       <p>Data do Agendamento: <strong>{post.date}</strong></p>
       <p>Observação: <strong>{post.body}</strong></p>
