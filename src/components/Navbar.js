557b1ed0e5c24956cd34f03acc6b994846828ff5
Guilherme Gomes <guifariasgomes@gmail.com>
2024-07-06 18:22:31 -0300

    media querie css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,87 +1,97 @@
-// Importações
+// Navbar.jsx
+
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
+import { FaCarAlt } from "react-icons/fa";
 import { useAuthentication } from "../hooks/useAuthentication";
 import { useAuthValue } from "../contexts/AuthContext";
 import styles from "./Navbar.module.css";
 
-
 const Navbar = () => {
-  // Desestruturação para obter a função de logout do hook de autenticação
   const { logout } = useAuthentication();
-  // Desestruturação para obter o usuário atual do contexto de autenticação
   const { user } = useAuthValue();
+  const [showMenu, setShowMenu] = useState(false);
+  const [isRotated, setIsRotated] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    setIsRotated(!isRotated);
+  };
+
+  const handleNavLinkClick = () => {
+    setShowMenu(false);
+  };
 
   return (
-    <nav className={styles.navbar}>
-      {/* Link para a página inicial com a marca "Car Washed" */}
-      <NavLink className={styles.brand} to="/">
-        Car <span>Washed</span>
-      </NavLink>
-      <ul className={styles.links_list}>
-        {/* Link para a página inicial */}
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            Home
-          </NavLink>
-        </li>
-        {/* Se o usuário não estiver logado, mostra os links de login e cadastro */}
+    <nav className={`${styles.navbar} ${showMenu ? styles.showMenu : ""}`}>
+      <div className={styles.brand}>
+        <NavLink to="/" onClick={() => setShowMenu(false)}>
+          Car <span>Washed</span>
+        </NavLink>
+      </div>
+      <div
+        className={`${styles.menuIcon} ${showMenu ? styles.rotate : ""}`}
+        onClick={toggleMenu}
+      >
+        <FaCarAlt />
+      </div>
+      <ul
+        ref={menuRef}
+        className={`${styles.links_list} ${showMenu ? styles.show : ""}`}
+      >
         {!user && (
           <>
             <li>
-              <NavLink
-                to="/login"
-                className={({ isActive }) => (isActive ? styles.active : "")}
-              >
+              <NavLink to="/login" onClick={handleNavLinkClick}>
                 Entrar
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/register"
-                className={({ isActive }) => (isActive ? styles.active : "")}
-              >
+              <NavLink to="/register" onClick={handleNavLinkClick}>
                 Cadastrar
               </NavLink>
             </li>
           </>
         )}
-        {/* Se o usuário estiver logado, mostra os links de agendar horário e dashboard */}
         {user && (
           <>
             <li>
-              <NavLink
-                to="/posts/create"
-                className={({ isActive }) => (isActive ? styles.active : "")}
-              >
+              <NavLink to="/posts/create" onClick={handleNavLinkClick}>
                 Agendar Horário
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/dashboard"
-                className={({ isActive }) => (isActive ? styles.active : "")}
-              >
+              <NavLink to="/dashboard" onClick={handleNavLinkClick}>
                 Dashboard
               </NavLink>
             </li>
           </>
         )}
-        {/* Link para a página sobre */}
         <li>
-          <NavLink
-            to="/about"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
+          <NavLink to="/about" onClick={handleNavLinkClick}>
             Sobre
           </NavLink>
         </li>
-        {/* Se o usuário estiver logado, mostra o botão de sair */}
         {user && (
           <li>
-            <button onClick={logout}>Sair</button>
+            <button onClick={() => { logout(); setShowMenu(false); }}>
+              Sair
+            </button>
           </li>
         )}
       </ul>
